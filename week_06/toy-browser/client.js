12da5a83f4749cfeebfe5d0f6355721655d0526a
Wind <zeyli0@163.com>
2020-10-05 22:07:59 +0800

    状态机，toy browser bodyParser 部分

--- week_06/toy-browser/client.js
+++ week_06/toy-browser/client.js
@@ -55,24 +55,144 @@ class Request {
   toString() {
     return `${this.method} ${this.path} HTTP/1.1\r${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\r ${this.bodyText}`
   }
-  
+
 }
 
 class ResponseParser {
   constructor() {
+    this.WAITING_STATUS_LINE = 0
+    this.WAITING_STATUS_LINE_END = 1
+    this.WAITING_HEADDER_NAME = 2
+    this.WAITING_HEADDER_SPACE = 3
+    this.WAITING_HEADDER_VALUE = 4
+    this.WAITING_HEADDER_LINE_END = 5
+    this.WAITING_HEADDER_BLOCK_END = 6
+    this.WAITING_BODY = 7
 
+    this.current = this.WAITING_STATUS_LINE
+    this.statusLine = ''
+    this.headers = {}
+    this.headerName = ''
+    this.headerValue = ''
+    this.bodyParser = null;
   }
+
+  get isFinished() {
+    return this.bodyParser && this.bodyParser.isFinished
+  }
+
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      headers:this.headers,
+      body: this.bodyParser.content.join('')
+    }
+  }
+
   receive(string) {
     for(let i=0; i< string.length; i++) {
       this.receiveChar(string.charAt(i))
     }
   }
   receiveChar(char) {
-
+    if(this.current === this.WAITING_STATUS_LINE) {
+      if(char === '\r') {
+        this.current = this.WAITING_HEADDER_LINE_END
+      } else {
+        this.statusLine += char
+      }
+    } else if(this.current === this.WAITING_STATUS_LINE_END) {
+      if(char === '\n') {
+        this.current = this.WAITING_HEADDER_NAME
+      }
+    } else if(this.current === this.WAITING_HEADDER_NAME) {
+      if(char === ':') {
+        this.current = this.WAITING_HEADDER_SPACE
+      } else if(char === '\r') {
+        this.current = this.WAITING_HEADDER_BLOCK_END
+        // node 默认的值为chunked
+        if(this.headers['Transfer-Encoding'] === 'chunked') {
+          this.bodyParser = new TrunkedBodyParser()
+        }
+      } else {
+        this.headerName += char
+      }
+    } else if(this.current === this.WAITING_HEADDER_SPACE) {
+      if(char === ' ') {
+        this.current = this.WAITING_HEADDER_VALUE
+      }
+    } else if(this.current === this.WAITING_HEADDER_VALUE) {
+      if(char === '\r') {
+        this.current = this.WAITING_HEADDER_LINE_END
+        this.headers[this.headerName] = this.headerValue
+        this.headerName = ''
+        this.headerValue = ''
+      } else {
+        this.headerValue += char
+      }
+    } else if(this.current === this.WAITING_HEADDER_LINE_END) {
+      if(char === '\n') {
+        this.current = this.WAITING_HEADDER_NAME
+      }
+    } else if(this.current === this.WAITING_HEADDER_BLOCK_END) {
+      if(char === '\n') {
+        this.current = this.WAITING_BODY
+      }
+    } else if(this.current === this.WAITING_BODY) {
+      console.log(char)
+      this.bodyParser.receiveChar(char)
+    }
   }
 }
 
+class TrunkedBodyParser {
+  constructor() {
+    this.WAITING_LENGTH = 0
+    this.WAITING_LENGTH_LINE_END = 1
+    this.READING_TRUNK = 2
+    this.WAITING_NEW_LINE = 3
+    this.WAITING_NEW_LINE_END = 4
+    this.length = 0
+    this.content = []
+    this.isFinished = false
+    this.current = this.WAITING_LENGTH
+  }
 
+  receiveChar(char) {
+    if(this.current === this.WAITING_LENGTH) {
+      if(char === '\r') {
+        if(this.length === 0) {
+          this.isFinished = true
+        }
+        this.current = this.WAITING_LENGTH_LINE_END
+      } else {
+        // 16进制
+        this.length *= 16
+        this.length += parserInt(char, 16)
+      }
+    } else if(this.current === this.WAITING_LENGTH_LINE_END) {
+      if(char === '\n') {
+        this.current = this.READING_TRUNK
+      }
+    } else if(this.current === this.READING_TRUNK) {
+      this.content.push(char)
+      this.length--
+      if(this.length === 0) {
+        this.current = this.WAITING_NEW_LINE
+      }
+    } else if(this.current === this.WAITING_NEW_LINE) {
+      if(char === '\r') {
+        this.current = this.WAITING_NEW_LINE_END
+      }
+    } else if(this.current === this.WAITING_NEW_LINE_END) {
+      if(char === '\n') {
+        this.current = this.WAITING_LENGTH
+      }
+    }
+  }
+}
 
 void async function() {
   let request = new Request({
